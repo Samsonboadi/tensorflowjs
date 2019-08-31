72cd4e1b0f421a6529613ffc51ee6402ccb81942
samson-merkator <41911495+samson-merkator@users.noreply.github.com>
2019-08-31 06:45:48 +0200

    update tensorflowjs

--- index.js
+++ index.js
@@ -1,64 +1,6 @@
-const classifier = knnClassifier.create();
-const webcamElement = document.getElementById('webcam');
 let net;
-
-async function setupWebcam() {
-  return new Promise((resolve, reject) => {
-    const navigatorAny = navigator;
-    navigator.getUserMedia = navigator.getUserMedia ||
-        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-        navigatorAny.msGetUserMedia;
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({video: true},
-        stream => {
-          webcamElement.srcObject = stream;
-          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
-        },
-        error => reject());
-    } else {
-      reject();
-    }
-  });
-}
-
-
-
-
-/*async function app() {
-  console.log('Loading mobilenet..');
-
-  // Load the model.
-  net = await mobilenet.load();
-  console.log('Sucessfully loaded model');
-
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
-}*/
-
-/*async function app() {
-  console.log('Loading mobilenet..');
-
-  // Load the model.
-  net = await mobilenet.load();
-  console.log('Sucessfully loaded model');
-  
-  await setupWebcam();
-  while (true) {
-    const result = await net.classify(webcamElement);
-
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
-
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
-    await tf.nextFrame();
-  }
-}*/
-
+const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -84,6 +26,7 @@ async function app() {
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  //document.getElementById('class-d').addEventListener('click', () => addExample(3));
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
@@ -103,4 +46,22 @@ async function app() {
   }
 }
 
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
 app();
